6b2bf2dfee371334798c08eade8f7fab97ccbb2a
hossein karbasi <hoseinemto@gmail.com>
2022-05-26 18:59:01 +0430

    Create SettingPreferencesKey object

---
+++ app/src/main/java/com/example/eshop/data/local/data_store/SettingPreferencesKey.kt
@@ -0,0 +1,9 @@
+package com.example.eshop.data.local.data_store
+
+import androidx.datastore.preferences.core.stringPreferencesKey
+
+object SettingPreferencesKey {
+
+    val KEY_THEME = stringPreferencesKey("Preferences_theme")
+
+}
