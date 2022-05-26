72723663a24ba18815680b1707882fc1d7ab0c29
hossein karbasi <hoseinemto@gmail.com>
2022-05-26 19:57:48 +0430

    Create SettingsFragment

--- app/src/main/java/com/example/eshop/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/eshop/ui/activity/MainActivity.kt
@@ -2,17 +2,22 @@ package com.example.eshop.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.eshop.R
 import com.example.eshop.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val viewModel by viewModels<MainViewModel>()
     private val navController by lazy {
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_fragment) as NavHostFragment
@@ -27,5 +32,16 @@ class MainActivity : AppCompatActivity() {
 
         binding.bottomNavigationView.setupWithNavController(navController)
 
+        getPreferences()
+
     }
+
+    private fun getPreferences() {
+        lifecycleScope.launch {
+            viewModel.preferences.collect {
+
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/eshop/ui/fragments/settings/SettingsFragment.kt
@@ -0,0 +1,23 @@
+package com.example.eshop.ui.fragments.settings
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.eshop.R
+import com.example.eshop.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentSettingsBinding.bind(view)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
