cc74d0582a04ccbee4b92f40613728a9d873dc2c
hossein karbasi <hoseinemto@gmail.com>
2022-05-28 21:07:31 +0430

    Add LottieAnimationView

--- app/src/main/java/com/example/eshop/ui/fragments/product/ProductFragment.kt
+++ app/src/main/java/com/example/eshop/ui/fragments/product/ProductFragment.kt
@@ -3,6 +3,7 @@ package com.example.eshop.ui.fragments.product
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.core.text.HtmlCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -10,6 +11,7 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import androidx.viewpager2.widget.ViewPager2
 import com.example.eshop.R
+import com.example.eshop.data.remote.model.Product
 import com.example.eshop.databinding.FragmentProductBinding
 import com.example.eshop.utils.Result
 import com.example.eshop.utils.collectWithRepeatOnLifecycle
@@ -32,34 +34,69 @@ class ProductFragment : Fragment(R.layout.fragment_product) {
         setUpViewPager()
     }
 
-    @SuppressLint("SetTextI18n")
     private fun getProduct() = binding.apply {
         viewModel.getProduct(args.productId)
         viewModel.getProduct.collectWithRepeatOnLifecycle(viewLifecycleOwner) {
             when (it) {
-                is Result.Error -> {}
-                is Result.Loading -> {}
+                is Result.Error -> {
+                    isError(it.message.toString())
+                }
+                is Result.Loading -> {
+                    isLoading()
+                }
                 is Result.Success -> {
-                    productDetails = it.data
-                    productRating.text =
-                        "(${it.data?.rating_count.toString()}) ${it.data?.average_rating}"
-                    ratingBar.rating = it.data?.average_rating?.toFloat()!!
-                    productDec.text = it.data.let { it1 ->
-                        HtmlCompat.fromHtml(
-                            it1.description,
-                            HtmlCompat.FROM_HTML_MODE_LEGACY
-                        )
-                    }
-                    imageViewPagerAdapter.submitList(it.data.images)
+                    it.data?.let { it1 -> isSuccess(it1) }
                 }
             }
         }
+    }
+
+    private fun isLoading() = binding.apply {
+        loading.visible()
+        headerCardView.gone()
+        footerCardView.gone()
+        loading.playAnimation()
+    }
+
+    private fun isError(errorMessage: String) = binding.apply {
+        loading.visible()
+        headerCardView.gone()
+        footerCardView.gone()
+        loading.playAnimation()
+        Toast.makeText(requireContext(), errorMessage, Toast.LENGTH_SHORT).show()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun isSuccess(data:Product) = binding.apply {
+        loading.gone()
+        headerCardView.visible()
+        footerCardView.visible()
+        loading.pauseAnimation()
+        productDetails = data
+        productRating.text =
+            "(${data.rating_count}) ${data.average_rating}"
+        ratingBar.rating = data.average_rating.toFloat()
+        productDec.text = data.let { it1 ->
+            HtmlCompat.fromHtml(
+                it1.description,
+                HtmlCompat.FROM_HTML_MODE_LEGACY
+            )
+        }
+        imageViewPagerAdapter.submitList(data.images)
     }
 
     private fun setUpViewPager() {
         imageViewPagerAdapter = ImageViewPagerAdapter()
         binding.viewPager.adapter = imageViewPagerAdapter
         binding.viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+    }
+
+    private fun View.visible() {
+        visibility = View.VISIBLE
+    }
+
+    private fun View.gone() {
+        visibility = View.GONE
     }
 
     override fun onDestroyView() {
